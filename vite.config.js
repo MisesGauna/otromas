3c6619f32f8661797607a22cb630be64924af58b
Moises Alfredo Gauna <162836156+MisesGauna@users.noreply.github.com>
2024-12-09 12:59:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
+
+// vite.config.js  
+export default {  
+  server: {  
+    host: true // Esto hace que Vite escuche en todas las interfaces  
+  }  
+};  
